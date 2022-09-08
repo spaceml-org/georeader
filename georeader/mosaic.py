2b0ded6728ab86c2dd4d4cce564a4150fcf0c825
gonzalo <gonzmg88@gmail.com>
2022-09-08 14:30:12 +0200

    mosaic function WIP

--- georeader/mosaic.py
+++ georeader/mosaic.py
@@ -1,13 +1,22 @@
 from georeader.abstract_reader import GeoData
 from georeader.geotensor import GeoTensor
-from typing import List, Optional, Tuple
+from typing import List, Optional, Tuple, Union
 import rasterio.warp
+from georeader import read
 from georeader.read import read_reproject
 import numpy as np
+from georeader import window_utils
+from georeader import slices
+from shapely.geometry import Polygon, MultiPolygon, box
+import rasterio.windows
+from collections import namedtuple
 
-def spatial_mosaic(data_list:List[GeoData],
+def spatial_mosaic(data_list:Union[List[GeoData], List[Tuple[GeoData,GeoData]]],
+                   polygon:Optional[Polygon]=None,
+                   dst_transform:Optional[rasterio.transform.Affine]=None,
                    bounds:Optional[Tuple[float, float, float, float]]=None,
                    dst_crs:Optional[str]=None,
+                   window_size: Tuple[int, int]= (512, 512),
                    resampling:rasterio.warp.Resampling=rasterio.warp.Resampling.cubic_spline,
                    dst_nodata:Optional[int]=None) -> GeoTensor:
     """
@@ -18,10 +27,14 @@ def spatial_mosaic(data_list:List[GeoData],
     This function is very similar to `rasterio.merge.merge`.
 
     Args:
-        data_list: List of raster objects
-        bounds: bounds to compute the mosaic. If not provided it will use the union of the bounds of all the products.
+        data_list: List of raster objects. each element could be a single geodata object or a tuple of an object and a
+            mask (second item will be considered the invalid values mask).
+        polygon: polygon to compute the mosaic in dst_crs
+        bounds: bounds to compute the mosaic.
         dst_crs: CRS of the product. If not provided it will use the CRS of the first product of the list
-        resampling:s pecifies how data is reprojected from `rasterio.warp.Resampling`.
+        dst_transform: Optional dest transform. If not provided the dst_transform is a rectilinear transform computed
+        window_size: The mosaic will be computed by windows of this size (for efficiency purposes)
+        resampling:specifies how data is reprojected from `rasterio.warp.Resampling`.
         dst_nodata: no data value. if None will use `data_list[0].fill_value_default`
 
     Returns:
@@ -31,36 +44,144 @@ def spatial_mosaic(data_list:List[GeoData],
 
     assert len(data_list) > 0, f"Expected at least one product found 0 {data_list}"
 
-    if bounds is None:
-        # Union of all bounds
-        xs,ys = [], []
-        for data in data_list:
-            left, bottom, right, top = data.bounds
-            xs.extend([left, right])
-            ys.extend([bottom, top])
-        bounds = min(xs), min(ys), max(xs), max(ys)
+    if isinstance(data_list[0], tuple):
+        first_data_object =  data_list[0][0]
+        first_mask_object = data_list[0][1]
+    else:
+        first_data_object = data_list[0]
+        first_mask_object = None
+
+    if polygon is None:
+        if bounds is not None:
+            polygon = box(*bounds)
+        else:
+            for data in data_list:
+                if isinstance(data, tuple):
+                    data = data[0]
+                window_data = rasterio.windows.Window(row_off=0, col_off=0,
+                                                      width=data.shape[-1], height=data.shape[-2])
+                polygon_iter = window_utils.window_polygon(window_data, data.transform)
+                if dst_crs is None:
+                    dst_crs = data.crs
+                if not read.compare_crs(dst_crs, data.crs):
+                    polygon_iter = read.polygon_to_crs(polygon_iter, crs_polygon=data.crs, dst_crs=dst_crs)
+
+                if polygon is None:
+                    polygon = polygon_iter
+                else:
+                    polygon = polygon.union(polygon_iter)
+
+    if dst_transform is None:
+        dst_transform = first_data_object.transform
 
     if dst_crs is None:
-        dst_crs = data_list[0].crs
+        dst_crs = first_data_object.crs
 
-    resolution_dst_crs = data_list[0].res
+    GeoDataFake = namedtuple("GeoDataFake", ["transform","crs"])
+    window_polygon = read.window_from_polygon(GeoDataFake(transform=dst_transform, crs=dst_crs),
+                                              polygon, crs_polygon=dst_crs)
 
-    dst_nodata = dst_nodata or data_list[0].fill_value_default
-    data_return = read_reproject(data_list[0], bounds=bounds, dst_crs=dst_crs,
-                                 resolution_dst_crs=resolution_dst_crs, resampling=resampling,
+    # Shift transform to window
+    dst_transform = rasterio.windows.transform(window_polygon, transform=dst_transform)
+    dst_nodata = dst_nodata or first_data_object.fill_value_default
+
+    # Get object to save the results
+    data_return = read_reproject(first_data_object,
+                                 dst_crs=dst_crs, dst_transform=dst_transform,
+                                 resampling=resampling,
+                                 window_out=rasterio.windows.Window(row_off=0, col_off=0, width=window_polygon.width,
+                                                                    height=window_polygon.height),
                                  dst_nodata=dst_nodata)
+
+    # TODO invalid_values of spatial locations only
     invalid_values = data_return.values == dst_nodata
+
+    if first_mask_object is not None:
+        invalid_geotensor = read_reproject(first_mask_object,
+                                           dst_crs=dst_crs, dst_transform=dst_transform,
+                                           resampling=rasterio.warp.Resampling.nearest,
+                                           window_out=rasterio.windows.Window(row_off=0, col_off=0,
+                                                                              width=window_polygon.width,
+                                                                              height=window_polygon.height),
+                                           dst_nodata=dst_nodata)
+        invalid_geotensor.values = invalid_geotensor.values.astype(bool)
+        invalid_geotensor.values =  invalid_geotensor.values.squeeze()
+        assert len(invalid_geotensor.shape) == 2, f"Expected two dims, found {invalid_geotensor.shape}"
+        invalid_values|= invalid_geotensor.values
+
+    # TODO mask only has spatial locations!
+    data_return.values[invalid_values] = data_return.fill_value_default
+
     if not np.any(invalid_values):
         return data_return
 
-    for data in data_list[1:]:
-        data_read = read_reproject(data, bounds=bounds, dst_crs=dst_crs,
-                                   resolution_dst_crs=resolution_dst_crs, resampling=resampling,
-                                   dst_nodata=dst_nodata)
-        assert data_read.shape == data_return.shape, f"Different read shapes for product {data} {data_read.shape} and {data_list[0]} {data_return.shape}"
-        data_return.values[invalid_values] = data_read.values[invalid_values]
-        invalid_values = data_return.values == dst_nodata
-        if not np.any(invalid_values):
-            return data_return
+    windows = slices.create_windows(data_return, window_size)
+
+    polygons_geodata = []
+    for window in windows:
+        slice_spatial = window.toslices()
+        invalid_values_window = invalid_values[slice_spatial]
+        if not np.any(invalid_values_window):
+            continue
+
+        # TODO add dims to slice_obj
+        slice_obj = (slice(None),) + slice_spatial
+        dst_transform_iter = rasterio.windows.transform(window, transform=dst_transform)
+        window_reproject_iter = rasterio.windows.Window(row_off=0, col_off=0, width=window.width, height=window.height)
+        polygon_iter = window_utils.window_polygon(window, dst_transform)
+
+        for _i,data in enumerate(data_list[1:]):
+            if isinstance(data, tuple):
+                geodata = data[0]
+                geomask = data[1]
+            else:
+                geodata = data
+                geomask = None
+
+            if polygons_geodata[_i] is None:
+                polygon_geodata = window_utils.window_polygon(rasterio.windows.Window(row_off=0, col_off=0,
+                                                                                      width=geodata.shape[-1], height=geodata.shape[-2]),
+                                                              geodata.transform)
+                if not read.compare_crs(dst_crs, geodata.crs):
+                    polygon_geodata = read.polygon_to_crs(polygon_geodata, crs_polygon=geodata.crs, dst_crs=dst_crs)
+            else:
+                polygon_geodata = polygons_geodata[_i]
+
+            if not polygon_geodata.intersects(polygon_iter):
+                continue
+
+            if geomask is not None:
+                invalid_geotensor = read_reproject(geomask,
+                                                   dst_crs=dst_crs, dst_transform=dst_transform_iter,
+                                                   resampling=rasterio.warp.Resampling.nearest,
+                                                   window_out=window_reproject_iter,
+                                                   dst_nodata=dst_nodata)
+                invalid_geotensor.values = invalid_geotensor.values.astype(bool)
+                invalid_values_iter = invalid_geotensor.values
+                invalid_geotensor.values = invalid_geotensor.values.squeeze()
+                assert len(invalid_geotensor.shape) == 2, f"Expected two dims, found {invalid_geotensor.shape}"
+                if np.all(invalid_values_iter):
+                    continue
+
+            data_read = read_reproject(geodata, dst_crs=dst_crs, window_out=window_reproject_iter,
+                                       dst_transform=dst_transform_iter, resampling=resampling,
+                                       dst_nodata=dst_nodata)
+            # TODO data_read could have more dims
+            masked_values_read = data_read.values == dst_nodata
+            if geomask is not None:
+                invalid_values_iter |= masked_values_read
+            else:
+                invalid_values_iter = masked_values_read
+
+            # TODO mask only has spatial locations!
+            mask_values_copy_out = invalid_values_window & ~invalid_values_iter
+
+            data_return.values[slice_obj][mask_values_copy_out] = data_read.values[mask_values_copy_out]
+
+            invalid_values_window &= invalid_values_iter
+
+            if not np.any(invalid_values_window):
+                break
+
 
     return data_return
