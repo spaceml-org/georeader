00a411b96c849c67035f20a5236f95423a97ac28
Gonzalo Mateo <gonzmg88@gmail.com>
2023-02-07 08:53:36 +0100

    Add version in setup

--- setup.py
+++ setup.py
@@ -1,5 +1,6 @@
 from setuptools import setup, find_packages
-
+import codecs
+import os.path
 
 def parse_requirements_file(filename):
     with open(filename, encoding="utf-8") as fid:
@@ -15,8 +16,21 @@ EXTRAS = {
     "docs": [ ],
 }
 
+def read(rel_path):
+    here = os.path.abspath(os.path.dirname(__file__))
+    with codecs.open(os.path.join(here, rel_path), 'r') as fp:
+        return fp.read()
+def get_version(rel_path):
+    for line in read(rel_path).splitlines():
+        if line.startswith('__version__'):
+            delim = '"' if '"' in line else "'"
+            return line.split(delim)[1]
+    else:
+        raise RuntimeError("Unable to find version string.")
+
+
 setup(name="georeader",
-      version="0.0.1",
+      version=get_version("georeader/__init__.py"),
       author="Gonzalo Mateo-Garcia",
       packages=find_packages(".", exclude=["tests"]),
       description="Lightweight thead and process save readers for big rasters",
