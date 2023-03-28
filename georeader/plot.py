77e9dd9c7c969f0ce282b4e8d035d9e24645229c
gonzalo <gonzmg88@gmail.com>
2023-03-28 12:33:40 +0200

    remove from here

--- georeader/plot.py
+++ georeader/plot.py
@@ -6,22 +6,6 @@ from typing import Optional, List, Union
 import matplotlib
 import rasterio.plot as rasterioplt
 
-COLORS_CLOUDSEN12 = np.array([[139, 64, 0], # clear
-                              [220, 220, 220], # Thick cloud
-                              [180, 180, 180], # Thin cloud
-                              [60, 60, 60]], # cloud shadow
-                             dtype=np.float32) / 255
-
-INTERPRETATION_CLOUDSEN12 = ["clear", "Thick cloud", "Thin cloud", "Cloud shadow"]
-
-INTERPRETATION_DYNAMIC_WORLD = ["water", "trees", "grass", "flooded_vegetation",  "crops", "shrub_and_scrub", "built", "bare", "snow_and_ice"]
-COLORS_DYNAMIC_WORLD_HEX = ["0000FF","007700", "88B053","42f5e0", "E49635", "DFC35A", "C4281B","A59B8F","B39FE1"]
-COLORS_DYNAMIC_WORLD = np.array([tuple(int(c[i:i+2], 16) for i in (0, 2, 4)) for c in COLORS_DYNAMIC_WORLD_HEX])/255
-
-COLORS_DYNAMIC_WORLD_CLOUDS_HEX = ["0000FF","007700", "88B053","42f5e0", "E49635", "DFC35A", "C4281B","A59B8F","B39FE1", "000000"]
-COLORS_DYNAMIC_WORLD_CLOUDS = np.array([tuple(int(c[i:i+2], 16) for i in (0, 2, 4)) for c in COLORS_DYNAMIC_WORLD_CLOUDS_HEX])/255
-INTERPRETATION_DYNAMIC_WORLD_CLOUDS = INTERPRETATION_DYNAMIC_WORLD + ["cloud"]
-
 
 def show(data:GeoData, **kwargs) -> matplotlib.axes.Axes:
     return rasterioplt.show(data.values, transform=data.transform, **kwargs)
@@ -69,40 +53,3 @@ def plot_segmentation_mask(mask:Union[GeoData, np.array], color_array:np.array,
         ax.legend(handles=patches,
                   loc='upper right')
     return ax
-
-
-def plot_cloudSEN12mask(mask:Union[np.array, GeoData], legend: bool = True,
-                        ax:Optional[matplotlib.axes.Axes]=None) -> matplotlib.axes.Axes:
-    """
-    See https://cloudsen12.github.io/
-
-    Args:
-        mask: (H, W)
-        legend: plot the legend
-        ax: matplotlib.Axes to plot
-
-    Returns:
-        matplotlib.Axes
-    """
-
-    return plot_segmentation_mask(mask=mask, color_array=COLORS_CLOUDSEN12,
-                                  interpretation_array=INTERPRETATION_CLOUDSEN12, legend=legend, ax=ax)
-
-
-def plot_DynamicWorldmask(mask: Union[np.array, GeoData], legend: bool = True,
-                          ax: Optional[matplotlib.axes.Axes] = None) -> matplotlib.axes.Axes:
-    """
-    See https://github.com/google/dynamicworld
-
-    Args:
-        mask: (H, W)
-        legend: plot the legend
-        ax: matplotlib.Axes to plot
-
-    Returns:
-        matplotlib.Axes
-    """
-
-    return plot_segmentation_mask(mask=mask, color_array=COLORS_DYNAMIC_WORLD,
-                                  interpretation_array=INTERPRETATION_DYNAMIC_WORLD,
-                                  legend=legend, ax=ax)
